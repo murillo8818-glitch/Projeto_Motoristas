6c93993a8733e65c1e4ed2079ec844020d98124e
murillo8818-glitch <murillo8818@gmail.com>
2026-03-16 11:47:30 -0300

    Alinhado referencias e separação dos Arq.CSS

--- Murillo_DB_motoristas_Java/Html/Html/motoristas.js
+++ Murillo_DB_motoristas_Java/Html/Html/motoristas.js
@@ -1,3 +1,12 @@
 document.getElementById('open_btn').addEventListener('click',function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');     
 });
+ 
+
+// criando variavel 
+
+let Nomes = "Cachorro loko"
+let arrays = [54, "cachorro loko"]
+
+//Linguagem doida da cabeça 
+let lista = {"cachorro logo" : 54}
